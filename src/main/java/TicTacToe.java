b36aeb15d1a96a0943e7a3c931c4ef38c957e922
NavyaV637 <navyavraman@gmail.com>
2026-05-06 14:01:04 +0530

    uc7

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,6 +1,33 @@
+import java.util.Random;
+
 public class TicTacToe {
     public static void updateBoard(char[][] board, int row, int col, char symbol) {
         board[row][col] = symbol;
+    }
+    public static int[] convertSlotToPosition(int slot) {
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
+        return new int[]{row, col};
+    }
+    public static boolean isValidMove(char[][] board, int row, int col) {
+        return board[row][col] == '-';
+    }
+    public static void computerMove(char[][] board, char symbol) {
+        Random rand = new Random();
+        int slot, row, col;
+
+        while (true) {
+            slot = rand.nextInt(9) + 1; // 1–9
+            int[] pos = convertSlotToPosition(slot);
+            row = pos[0];
+            col = pos[1];
+
+            if (isValidMove(board, row, col)) {
+                updateBoard(board, row, col, symbol);
+                System.out.println("Computer chose slot: " + slot);
+                break;
+            }
+        }
     }
     public static void displayBoard(char[][] board) {
         for (int i = 0; i < 3; i++) {
@@ -16,11 +43,7 @@ public class TicTacToe {
                 {'-', '-', '-'},
                 {'-', '-', '-'}
         };
-
-        int row = 1;
-        int col = 1;
-        char symbol = 'X';
-        updateBoard(board, row, col, symbol);
+        computerMove(board, 'O');
         displayBoard(board);
     }
 }
