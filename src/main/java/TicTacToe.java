35130a49c60c0869298f9e4814285543c31faf2a
NavyaV637 <navyavraman@gmail.com>
2026-05-06 13:43:16 +0530

    UC3 Added

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,34 +1,14 @@
-import java.util.Random;
-
+import java.util.Scanner;
 public class TicTacToe {
+    public static int getSlotInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = sc.nextInt();
+        return slot;
+    }
     public static void main(String[] args) {
+        int selectedSlot = getSlotInput();
 
-        Random rand = new Random();
-        int toss = rand.nextInt(2);
-
-        String player1 = "Player 1";
-        String player2 = "Player 2";
-
-        char player1Symbol, player2Symbol;
-        String currentPlayer;
-        char currentSymbol;
-
-        if (toss == 0) {
-            currentPlayer = player1;
-            player1Symbol = 'X';
-            player2Symbol = 'O';
-            currentSymbol = player1Symbol;
-        } else {
-            currentPlayer = player2;
-            player2Symbol = 'X';
-            player1Symbol = 'O';
-            currentSymbol = player2Symbol;
-        }
-
-        System.out.println("Toss Result: " + toss);
-        System.out.println(currentPlayer + " will start first.");
-        System.out.println(player1 + " symbol: " + player1Symbol);
-        System.out.println(player2 + " symbol: " + player2Symbol);
-        System.out.println("Current Player: " + currentPlayer + " (" + currentSymbol + ")");
+        System.out.println("You selected slot: " + selectedSlot);
     }
 }
